5aaa57c1ebaeff62f8bd29134b7950543db74a92
maxravel <maxmaterniak@gmail.com>
2018-03-15 14:12:48 +0100

    gallery upradges

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 const slider=document.querySelector('.slider');
+const slide=document.querySelector('#slide');
+const photos = ['./img/gallery1.jpg', './img/gallery2.jpg']
 
-
+let i = 0;
 
 next.addEventListener('click', nextPhoto);
+
 function nextPhoto(e){
-    slider.style.backgroundImage = "url('./img/gallery1.jpg')";
-    //document.body.style.backgroundImage = "url('img_tree.png')";
-    console.log('gdfgd');
+
+    i<photos.length-1 ? i++ : i=0;
+    slide.src = photos[i];
+    e.preventDefault();
+
+}
+
+prev.addEventListener('click', prevPhoto);
+
+function prevPhoto(e){
+
+    i>0 ? i-- : i=photos.length-1;
+    slide.src = photos[i];
     e.preventDefault();
 
 }
